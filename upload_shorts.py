27aee338a3baebd695aeb5673874ca82decd64d5
pinky3052004-debug <pinky3052004@gmail.com>
2026-09-06 15:39:58 +0630

    Update upload_shorts.py

--- upload_shorts.py
+++ upload_shorts.py
@@ -140,14 +140,16 @@ def main():
         return
 
     # တစ်ကြိမ်လျှင် အများဆုံး ၅ ဖိုင်
-    videos_to_upload = pending_videos[:5]
+    videos_to_upload = pending_videos[:7]
     
     schedule_slots = [ 
-        (8, 30),
+        (7, 30),
+        (9, 30),
         (11, 30),
         (14, 30),
         (16, 30), 
-        (19, 30)   
+        (19, 30),
+        (21, 30)
     ]
 
     mmt_tz = timezone(timedelta(hours=6, minutes=30))
